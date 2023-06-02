637ee9e678d9b27d2c6299cbac2fc4297d8561f4
delicdenan <delicdenancs@gmail.com>
2023-06-02 21:37:18 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import { LandingPage } from "./LandingPage/LandingPage";
+import { Route, Routes } from "react-router-dom";
+import { Search } from "./Search/Search";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Routes>
+      <Route path="/search" element={<Search />} />
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,6 +4,7 @@ import { SearchBar } from "../SearchBar/SearchBar";
 import classes from "./LandingPage.module.css";
 import { WelcomeText } from "../WelcomeText/WelcomeText";
 import { Gallery } from "../Gallery/Galllery";
+
 export function LandingPage() {
   return (
     <div className={classes.landing}>
